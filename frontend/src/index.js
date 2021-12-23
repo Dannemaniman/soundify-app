021130aca4c8b43d9decff9c6708d22eee1d5cb7
Dannemaniman <daniellangas91@gmail.com>
2021-12-23 10:04:42 +0100

    Created basic file structure.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SoundifyContextProvider } from './store/soundify-context'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SoundifyContextProvider>
+      <App />
+    </SoundifyContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/store/soundify-context.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+const SoundifyContext = React.createContext({
+  user: true,
+  onLogin: () => { },
+  fetchFromAPI: () => {}
+})
+
+
+export const SoundifyContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const loginHandler = () => {
+    setIsLoggedIn(true)
+  }
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false)
+  }
+
+  const fetchFromAPI = () => {
+  }
+ 
+
+
+  return <SoundifyContext.Provider>{props.children}</SoundifyContext.Provider>
+}
+
+
+export default SoundifyContext
