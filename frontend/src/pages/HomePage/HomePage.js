1ddc5913100e156c0996ec3fa44c388867837b1c
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2021-12-26 19:38:09 +0100

    Added hero img component and most played list component

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,6 +1,23 @@
+import HeroImg from '../../components/heroImg/HeroImg'
+import MostPlayed from '../../components/landingPage/MostPlayed'
+import styles from './HomePage.module.css'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <>
+      <div className={styles.homepage}>
+        <HeroImg
+          imgUrl={
+            'https://img.freepik.com/free-vector/musical-notes-frame-with-text-space_1017-32857.jpg?size=626&ext=jpg'
+          }
+          size={'big'}
+          caption={'Landing page'}
+        />
+
+        <MostPlayed />
+      </div>
+    </>
+  )
 }
 
 export default HomePage
