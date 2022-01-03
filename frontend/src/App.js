2a1f86a69874c3bca3b51b3c86c0d9e9bccb5c7a
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2022-01-03 10:38:12 +0100

    working on the registration
    managed to create an object and save the desired credentials when creating a new account. What's left is to send the object through post to the server so it can save the user to the DB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,35 +4,35 @@ import Sidebar from './components/sidebar/Sidebar';
 import Login from './pages/LoginPage/LoginPage';
 import Home from './pages/HomePage/HomePage';
 import ArtistPage from './pages/ArtistPage/ArtistPage';
-import Signup from './pages/SignUpPage/SignUpPage';
+import Signup from './pages/SignupPage/SignUpPage';
 import { Route, Routes } from 'react-router-dom';
 import { useContext, useState } from 'react';
 import SoundifyContext from './store/soundify-context';
 
 const App = () => {
-  const [sidebar, setsidebar] = useState(false);
+	const [sidebar, setsidebar] = useState(false);
 
-  const showSidebar = () => {
-    setsidebar(!sidebar);
-  };
+	const showSidebar = () => {
+		setsidebar(!sidebar);
+	};
 
-  const ctx = useContext(SoundifyContext);
-  console.log(ctx.user);
+	const ctx = useContext(SoundifyContext);
+	console.log(ctx.user);
 
-  return (
-    <div className='App'>
-      <Header showSidebar={showSidebar} />
-      <Sidebar hideSidebar={showSidebar} animation={sidebar} />
-      <main className='main'>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/artist/:id' element={<ArtistPage />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </main>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header showSidebar={showSidebar} />
+			<Sidebar hideSidebar={showSidebar} animation={sidebar} />
+			<main className='main'>
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/artist/:id' element={<ArtistPage />} />
+					<Route path='/signup' element={<Signup />} />
+				</Routes>
+			</main>
+		</div>
+	);
 };
 
 export default App;
--- frontend/src/components/form/form.js
+++ frontend/src/components/form/form.js
@@ -1,47 +1,79 @@
-import form from './form.css';
+import './form.css';
+import { useState, useEffect } from 'react';
 
 const Form = () => {
-  const handelSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    for (let [key, value] of formData.entries()) {
-      console.log(key, value);
-    }
-  };
+	const defaultValues = {
+		username: '',
+		email: '',
+		password: '',
+	};
 
-  return (
-    <form onSubmit={handelSubmit} className='formContainer'>
-      <label className='labelContainer'>
-        <h1>Sign up</h1>
-        <h4>Username</h4>
-        <input type='text' name='username' />
-        <h4>Email</h4>
-        <input type='email' name='email' />
-        <h4>Password</h4>
-        <input type='password' name='password' />
-        <h4>Confirm password</h4>
-        <input type='password' name='confirmpassword' />
-      </label>
-      <input type='submit' value='Continue' className='submitBtn' />
+	const [newUser, setNewUser] = useState(defaultValues);
 
-      <div className='iconBar'>
-        <div className='icons'>
-          <i className='fab fa-facebook-f'></i>
-        </div>
-        <div className='icons'>
-          <i className='fab fa-twitter'></i>
-        </div>
-        <div className='icons'>
-          <i className='fab fa-google'></i>
-        </div>
-      </div>
-      <div className='login'>
-        <h3>
-          Already have an account? <a href='/login'>Login</a>
-        </h3>
-      </div>
-    </form>
-  );
+	//Här skickar vi objektet till backend genom en post antar jag eller något sådant:
+	// fetch("/home", { method: post, headers: { 'Content-type': "application/json" }, body: JSON.stringify(newUser) }).then(res => res.json()).then(data => console.log(data))
+
+	useEffect(() => {});
+
+	const handelSubmit = (event) => {
+		console.log(newUser);
+		event.preventDefault();
+	};
+
+	return (
+		<form onSubmit={handelSubmit} className='formContainer'>
+			<label className='labelContainer'>
+				<h1>Sign up</h1>
+				<h4>Username</h4>
+				<input
+					type='text'
+					name='username'
+					value={newUser.username}
+					onChange={(e) => {
+						setNewUser({ ...newUser, username: e.target.value });
+					}}
+				/>
+				<h4>Email</h4>
+				<input
+					type='email'
+					name='email'
+					value={newUser.email}
+					onChange={(e) => {
+						setNewUser({ ...newUser, email: e.target.value });
+					}}
+				/>
+				<h4>Password</h4>
+				<input
+					type='password'
+					name='password'
+					value={newUser.password}
+					onChange={(e) => {
+						setNewUser({ ...newUser, password: e.target.value });
+					}}
+				/>
+				<h4>Confirm password</h4>
+				<input type='password' name='confirmpassword' />
+			</label>
+			<input type='submit' value='Continue' className='submitBtn' />
+
+			<div className='iconBar'>
+				<div className='icons'>
+					<i className='fab fa-facebook-f'></i>
+				</div>
+				<div className='icons'>
+					<i className='fab fa-twitter'></i>
+				</div>
+				<div className='icons'>
+					<i className='fab fa-google'></i>
+				</div>
+			</div>
+			<div className='login'>
+				<h3>
+					Already have an account? <a href='/login'>Login</a>
+				</h3>
+			</div>
+		</form>
+	);
 };
 
 export default Form;
