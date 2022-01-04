bd52ff01534012564ce070b0ac84c86fae9afd3f
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2022-01-04 09:08:18 +0100

    Update playlistService.ts
    Started on creating methods in playlist controller

--- backend/src/services/playlistService.ts
+++ backend/src/services/playlistService.ts
@@ -1,1 +1,12 @@
 import { PlaylistInterface } from '../db/models/Playlist';
+import Playlist from '../db/schemas/playlistSchema';
+
+const createNewPlaylist = async (playlist: PlaylistInterface) => {
+	try {
+		const doc = new Playlist(playlist);
+		await doc.save();
+		return doc;
+	} catch (error) {
+		console.log(error);
+	}
+};
