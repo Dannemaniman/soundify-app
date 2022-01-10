f082b23522779af4b236da462de368ef91da2244
Dannemaniman <daniellangas91@gmail.com>
2022-01-10 02:46:31 +0100

    Resolved changes.

--- frontend/src/components/youtubePlayer/YoutubePlayer.js
+++ frontend/src/components/youtubePlayer/YoutubePlayer.js
@@ -1,88 +1,11 @@
-<<<<<<< HEAD
-import React, { useEffect, useState, useRef, useContext, useCallback, useMemo } from 'react'
-import styles from './YoutubePlayer.module.css'
-=======
 import React, { useEffect, useState, useRef, useContext } from 'react'
 import styles from './YoutubePlayer.module.css'
 import YouTube from 'react-youtube'
->>>>>>> 0daea0579d2636e098cfcf1e7c98b8d9d3c3100f
 import { PlayerContext } from '../../store/playerContext'
 
 const YoutubePlayer = (props) => {
   const context = useContext(PlayerContext)
 
-<<<<<<< HEAD
-  const progressBarDone = useRef(null)
-  const progressBar = useRef(null)
-  const timePlayed = useRef(null)
-
-  const [playing, setplaying] = useState(false)
-
-  const [song, setSong] = useState(null)
-
-  let durationInterval
-
-  let player
-
-  useEffect(() => {
-    if (!window.YT) {
-      const tag = document.createElement('script')
-      tag.src = 'https://www.youtube.com/iframe_api'
-
-      window.onYouTubeIframeAPIReady = loadPlayer
-
-      const firstScriptTag = document.getElementsByTagName('script')[0]
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
-    } else {
-      loadPlayer()
-    }
-  }, [song])
-
-    useEffect(() => {
-    if (!context.song) return
-    setSong(context.song)
-    console.log(context.song)
-  
-    setTimeout(() => {
-      if (player) {
-        player.loadVideoById(context.song.videoId)
-        setTimes()
-        }
-    }, 10)
-  }, [context.song])
-
-  const loadPlayer =  useCallback(() => {
-    player = new window.YT.Player(`yt-player`, {
-      videoId: song ? song.song.videoId : '',
-      events: {
-        onReady: useUrlParams,
-      },
-    })
-    console.log(player)
-  }, []) 
-
-
-
-  const setTimes = () => {
-    clearInterval(durationInterval)
-    durationInterval = setInterval(() => {
-      if (player.getCurrentTime() === null) return
-
-      let currentTime = Math.ceil(player.getCurrentTime())
-      let seconds =
-        currentTime % 60 < 10
-          ? '0' + Math.ceil(currentTime % 60)
-          : Math.ceil(currentTime % 60)
-      let time = `${Math.ceil(currentTime / 60 - 1)}:${seconds}`
-      if (time === '-1:00') {
-        timePlayed.current.innerHTML = '0:00'
-      } else {
-        timePlayed.current.innerHTML = time
-      }
-
-      progressBarDone.current.style.setProperty(
-        'width',
-=======
   const progressBar = useRef(null)
 
   const [playing, setplaying] = useState(false)
@@ -125,26 +48,11 @@ const YoutubePlayer = (props) => {
 
       setstartTime(getTime(player.getCurrentTime()))
       setdoneProgress(
->>>>>>> 0daea0579d2636e098cfcf1e7c98b8d9d3c3100f
         (player.getCurrentTime() / player.getDuration()) * 100 + '%'
       )
     }, 500)
   }
 
-<<<<<<< HEAD
-  function useUrlParams() {
-    if (song) {
-      setTimeout(() => {
-        player.loadVideoById(song.videoId);
-      }, 10);
-    }
-  }
-
-  const onPlayerReady = () => {
-    //setsong(context)
-    player.loadVideoById(context.song.videoId)
-    if (window.YT.PlayerState.PLAYING) setplaying(true)
-=======
   const onPlayerReady = (event) => {
     event.target.pauseVideo()
     setplayer(event.target)
@@ -201,7 +109,6 @@ const YoutubePlayer = (props) => {
       ':' +
       (seconds < 10 ? '0' + seconds : seconds)
     )
->>>>>>> 0daea0579d2636e098cfcf1e7c98b8d9d3c3100f
   }
 
   const pausePlayer = () => {
@@ -213,29 +120,8 @@ const YoutubePlayer = (props) => {
     setplaying(true)
   }
 
-<<<<<<< HEAD
-  const PlayPaus = () => {
-    if (!playing) {
-      return (
-        <button className={styles.next} onClick={startPlayer}>
-          <i className='fas fa-play'></i>
-        </button>
-      )
-    } else {
-      return (
-        <button className={styles.next} onClick={pausePlayer}>
-          <i className='fas fa-pause'></i>
-        </button>
-      )
-    }
-  }
-
-  //Change time on song with progress bar
-  function pickSongTime(e) {
-=======
   //Change time on song with progress bar
   function changedSongTime(e) {
->>>>>>> 0daea0579d2636e098cfcf1e7c98b8d9d3c3100f
     let coordStart = progressBar.current.getBoundingClientRect().left
     let coordEnd = e.pageX
     let percent = (coordEnd - coordStart) / progressBar.current.offsetWidth
@@ -243,10 +129,6 @@ const YoutubePlayer = (props) => {
     return player.getDuration() * percent
   }
 
-<<<<<<< HEAD
-  const pickTime = (e) => {
-    player.seekTo(pickSongTime(e), true)
-=======
   const changeTime = (e) => {
     player.seekTo(changedSongTime(e), true)
   }
@@ -265,22 +147,11 @@ const YoutubePlayer = (props) => {
       }
     }
     return css
->>>>>>> 0daea0579d2636e098cfcf1e7c98b8d9d3c3100f
   }
 
   return (
     <>
       {song ? (
-<<<<<<< HEAD
-        <div className={styles.playerContainer}>
-          <div id='yt-player' className={styles.container}></div>
-
-          <div className={styles.songContainer}>
-            <img className={styles.img} src={song.img?.url} alt='' />
-            <div className={styles.name}>
-              <p className={styles.artistName}>{song.artist?.name}</p>
-              <p className={styles.songName}>{song.name}</p>
-=======
         <div className={styles.playerContainer} style={hidecss()}>
           <YouTube
             videoId={song.song.videoId}
@@ -299,18 +170,11 @@ const YoutubePlayer = (props) => {
             <div className={styles.name}>
               <p className={styles.artistName}>{song.song.artist?.name}</p>
               <p className={styles.songName}>{song.song?.name}</p>
->>>>>>> 0daea0579d2636e098cfcf1e7c98b8d9d3c3100f
             </div>
           </div>
 
           <div className={styles.buttonContainer}>
             <button className={styles.next}>
-<<<<<<< HEAD
-              <i className='fas fa-step-backward'></i>
-            </button>
-            <PlayPaus />
-            <button className={styles.next}>
-=======
               <i className='fas fa-step-backward' onClick={prevSong}></i>
             </button>
 
@@ -326,27 +190,11 @@ const YoutubePlayer = (props) => {
             )}
 
             <button className={styles.next} onClick={nextSong}>
->>>>>>> 0daea0579d2636e098cfcf1e7c98b8d9d3c3100f
               <i className='fas fa-step-forward'></i>
             </button>
           </div>
 
           <div className={styles.progressContainer}>
-<<<<<<< HEAD
-            <span className='start'></span>
-            <p ref={timePlayed}>0:40</p>
-            <div
-              className={styles.progressBar}
-              ref={progressBar}
-              onClick={pickTime}
-            >
-              <div
-                className={styles.progressBarDone}
-                ref={progressBarDone}
-              ></div>
-            </div>
-            <p id='time-duration'>3:42</p>
-=======
             <p>{startTime}</p>
             <div
               className={styles.progressBar}
@@ -359,7 +207,6 @@ const YoutubePlayer = (props) => {
               ></div>
             </div>
             <p>{endTime}</p>
->>>>>>> 0daea0579d2636e098cfcf1e7c98b8d9d3c3100f
           </div>
         </div>
       ) : (
