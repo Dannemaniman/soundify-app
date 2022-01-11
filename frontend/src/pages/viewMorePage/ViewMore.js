e867b9c817dec4af0e25aa13471c73e1f318ed0a
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2022-01-11 18:48:26 +0100

    removed logs

--- frontend/src/pages/viewMorePage/ViewMore.js
+++ frontend/src/pages/viewMorePage/ViewMore.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from 'react-router-dom'
 import s from './ViewMore.module.css'
 import backIcon from '../../assets/icons/back.png'
 import PlayBtn from '../../components/songlist/PlayBtn'
 
 const ViewMore = () => {
-
   let navigate = useNavigate()
 
   const query = new URLSearchParams(useLocation().search)
-  const type = query.get("query")
-  const name = query.get("name")
+  const type = query.get('query')
+  const name = query.get('name')
 
   const [dataToRender, setDataToRender] = useState([])
   const [isLoading, setIsLoading] = useState(false)
@@ -35,31 +34,51 @@ const ViewMore = () => {
     navigate(`/search/${name}`, { replace: true })
   }
 
-
   return (
     <div className={s.container}>
-      {console.log(dataToRender)}
-      <div className={s.header} onClick={() => { handleGoback() }}>
-        <img src={backIcon} alt="" />
-        <h1>{type.charAt(0).toUpperCase()}{type.slice(1)} found:</h1>
+      <div
+        className={s.header}
+        onClick={() => {
+          handleGoback()
+        }}
+      >
+        <img src={backIcon} alt='' />
+        <h1>
+          {type.charAt(0).toUpperCase()}
+          {type.slice(1)} found:
+        </h1>
       </div>
-      {isLoading && <div className={isLoading ? "loader" : ""}></div>}
-      {!isLoading && dataToRender.map((ele, index) => {
-        return (
-          <div className={s.viewMoreItem} key={index}>
-            <div className={s.mainContent}>
-              <h1 className={ele.type !== 'song' ? s.artistTitle : s.songTitle}>{ele.name.substring(0, 20)} {ele.name.length > 20 ? "..." : ""}</h1>
-              {ele.type !== 'song' && <img src={ele.thumbnails[0].url} alt="artist or album" />}
-              {ele.type === 'song' && <PlayBtn songs={dataToRender} index={index}
-                song={ele} thumbnails={ele.thumbnails} />}
+      {isLoading && <div className={isLoading ? 'loader' : ''}></div>}
+      {!isLoading &&
+        dataToRender.map((ele, index) => {
+          return (
+            <div className={s.viewMoreItem} key={index}>
+              <div className={s.mainContent}>
+                <h1
+                  className={ele.type !== 'song' ? s.artistTitle : s.songTitle}
+                >
+                  {ele.name.substring(0, 20)}{' '}
+                  {ele.name.length > 20 ? '...' : ''}
+                </h1>
+                {ele.type !== 'song' && (
+                  <img src={ele.thumbnails[0].url} alt='artist or album' />
+                )}
+                {ele.type === 'song' && (
+                  <PlayBtn
+                    songs={dataToRender}
+                    index={index}
+                    song={ele}
+                    thumbnails={ele.thumbnails}
+                  />
+                )}
+              </div>
+              {typeof ele.artist === 'string' && <p>By: {ele.artist}</p>}
+              {!ele.artist && <p>Go to artist page</p>}
             </div>
-            {typeof ele.artist === 'string' && <p>By: {ele.artist}</p>}
-            {!ele.artist && <p>Go to artist page</p>}
-          </div>)
-      })}
+          )
+        })}
     </div>
   )
-
 }
 
 export default ViewMore
