e8bb358f482bcfefc3e4b30fd6974165fd479ea4
Dannemaniman <daniellangas91@gmail.com>
2022-01-24 12:12:18 +0100

    Fixed bug.

--- backend/src/services/searchService.ts
+++ backend/src/services/searchService.ts
@@ -16,8 +16,14 @@ const fetchFromHeroku = async (type: string, query: string, next?: string) => {
   const next2 = next ? `?next=${next}` : ""
   const path = `${process.env.HEROKU}${type}/${query}${next2}`
 
+  console.log(path)
+
+
   let response = await fetch(path)
+  console.log(response)
+
   const res = await response.json()
+  console.log(res)
 
   return res
 }
